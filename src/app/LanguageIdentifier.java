32d3e98ba66abad607db9c1d02eb699f6651bbdc
Connor Bode <connor.bode@gmail.com>
2014-03-08 10:48:40 -0500

    LanguageIdentifier now works - can train multiple language identifiers - can evaluate a string and decide which language the string belogns to

--- src/app/LanguageIdentifier.java
+++ src/app/LanguageIdentifier.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Hashtable;
+import java.util.ArrayList;
 
 public class LanguageIdentifier {
 
@@ -11,7 +12,11 @@ public class LanguageIdentifier {
 		VARIABLES
 	   =============================================== */
 
+	// The hashtable of bigrams
 	private Hashtable<String, Bigram> bigrams;
+	
+	// The list of hashtable keys
+	private ArrayList<String> keys;
 	
 	/* ===============================================
 		CONSTRUCTORS
@@ -20,19 +25,63 @@ public class LanguageIdentifier {
 	public LanguageIdentifier() {
 		
 		bigrams = new Hashtable<String, Bigram>();
+		keys = new ArrayList<String>();
 	}
 	
 	/* ===============================================
 		METHODS
 	   =============================================== */
 	
-	public void addCorpus(String key, File file) throws FileNotFoundException, IOException, CharacterSetException {
+	/**
+	 * Trains one of the language identifiers
+	 * @param key the key of the language identifier to train
+	 * @param file the file containing the training text
+	 * @throws FileNotFoundException if the file specified could not be found
+	 * @throws IOException if the file specified was inaccessible
+	 * @throws CharacterSetException if the file specified contained unrecognized characters
+	 */
+	public void train(String key, File file) throws FileNotFoundException, IOException, CharacterSetException {
 		
 		// If the bigram doesn't exist, create it
-		if(!bigrams.containsKey(key))
+		if(!bigrams.containsKey(key)) {
 			bigrams.put(key, new Bigram(file));
+			addKey(key);
+		}
 		
 		// If the bigram does exist, update the corpus
-		else {}
+		else
+			bigrams.get(key).train(file);
+	}
+	
+	/**
+	 * Evaluates a string and decides the langauge of the string
+	 * @param s the string to be identified
+	 * @return the predicted language to which the string belongs
+	 * @throws CharacterSetException if there are unrecognized characters in the string
+	 */
+	public String evaluate(String s) throws CharacterSetException {
+		
+		// default text if there are no hashtable keys
+		String bestKey = "no language identifiers have been trained";
+		double bestValue = -Double.MAX_VALUE;
+		
+		// Iterate all hashtable keys to see which is the greatest
+		for(String key : keys) {
+			double value = bigrams.get(key).evaluate(s);
+			if(Double.compare(value, bestValue) > 0) {
+				bestKey = key;
+				bestValue = value;
+			}
+		}
+		
+		return bestKey;
+	}
+	
+	/**
+	 * Records a bigram hashtable key for later use
+	 * @param key the key to the bigram hashtable
+	 */
+	public void addKey(String key) {
+		keys.add(key);
 	}
 }
--- src/app/Main.java
+++ src/app/Main.java
@@ -7,14 +7,11 @@ public class Main {
 	public static void main(String[] args) {
 		
 		try {
-			Bigram english = new Bigram(new File("files/english.txt"));
-			Bigram french = new Bigram(new File("files/french.txt"));
-			String test = "Le printemps se fait attendre.";
-			double probEnglish = english.evaluate(test);
-			double probFrench = french.evaluate(test);
-			System.out.println("Probability English: " + probEnglish);
-			System.out.println("Probability French: " + probFrench);
-			System.out.println("The text is " + (probEnglish > probFrench ? "english" : "french"));
+			LanguageIdentifier li = new LanguageIdentifier();
+			li.train("english",  new File("files/english.txt"));
+			li.train("french", new File("files/french.txt"));
+			String best = li.evaluate("Le printemps se fait attendre.");
+			System.out.println("the sentence is " + best);
 		}
 		
 		catch(Exception e) {
